4df3790a88d07178f4838f82871b8fa990096cb5
innowavesolution <innowavesolutioninfo@gmail.com>
2026-05-25 15:39:18 +0600

    fix: add missing columns in tbl_products to migration script

--- src/migration.controller.ts
+++ src/migration.controller.ts
@@ -10,6 +10,15 @@ export class MigrationController {
     try {
       await this.dataSource.query('ALTER TABLE system_users ADD COLUMN IF NOT EXISTS "paidTotalSoldQty" integer NOT NULL DEFAULT 0');
       await this.dataSource.query('ALTER TABLE system_users ADD COLUMN IF NOT EXISTS "paidTotalEarning" numeric(12,2) NOT NULL DEFAULT 0');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "sizes" text');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "variants" text');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "types" text');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "weight" numeric(10,2)');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "length" numeric(10,2)');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "breadth" numeric(10,2)');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "width" numeric(10,2)');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "unit" character varying DEFAULT \'Piece\'');
+      await this.dataSource.query('ALTER TABLE tbl_products ADD COLUMN IF NOT EXISTS "variantId" text');
       await this.dataSource.runMigrations();
       return { success: true, message: "Migrations run successfully on Vercel!" };
     } catch (e) {
